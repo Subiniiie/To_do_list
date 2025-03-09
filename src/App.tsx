320e5b0b2b1d6ac8160e5d518c916a82a316d3bb
Subiniiie <subin57419@gmail.com>
2025-03-09 14:45:05 +0900

    배경이미지 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BackgroundWrapper } from "./styles/backgroundImage";
+
 function App() {
 
   return (
-    <>
-    </>
+    <BackgroundWrapper>
+    </BackgroundWrapper>
   )
 }
 
---
+++ src/styles/backgroundImage.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import backgroundImage from "../assets/background.jpg";
+
+export const BackgroundWrapper = styled.div`
+    background: url(${backgroundImage});
+    background-size: cover;
+    height: 100vh;
+    margin: 0;
+    opacity: 0.8;
+`
