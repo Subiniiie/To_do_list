8cb3349face515d76e76e6dd63c08252c6104566
Subiniiie <subin57419@gmail.com>
2025-03-11 22:31:40 +0900

    배경 색상 코드 수정

--- src/styles/container.ts
+++ src/styles/container.ts
@@ -4,7 +4,7 @@ import { color } from "../store/color";
 export const ContainerWrapper = styled.div<{ lightMode: boolean }>`
     width: 500px;
     height: 500px;
-    background-color: ${({ lightMode }) => (lightMode ? color.lmContainer : "#000000")};
+    background-color: ${({ lightMode }) => (lightMode ? color.lmContainer : color.dmContainer)};
     opacity: 0.7;
     border-radius: 15%;
 `
