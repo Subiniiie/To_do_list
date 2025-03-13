b58646bc0f2a1fa984ecdb9705c16528664f859d
Subiniiie <subin57419@gmail.com>
2025-03-13 20:37:18 +0900

    header 배치

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,15 +5,21 @@ import SubTitle from "./SubTitle";
 import SettingBtn from "./SettingBtn";
 import ToggleBtn from "./ToggleBtn";
 import Input from "./Input";
+import { HeaderContainer } from "../styles/header";
+import { BtnsContainer } from "../styles/Btns";
 
 const Container = () => {
     const { lightMode } = useLightMode();
     
     return (
         <ContainerWrapper lightMode={lightMode}>
-            <Title />
-            <SettingBtn />
-            <ToggleBtn />
+            <HeaderContainer>
+                <Title />
+                <BtnsContainer>
+                    <SettingBtn />
+                    <ToggleBtn />
+                </BtnsContainer>
+            </HeaderContainer>
             <SubTitle />
             <Input />
         </ContainerWrapper>
---
+++ src/styles/Btns.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const BtnsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+`
