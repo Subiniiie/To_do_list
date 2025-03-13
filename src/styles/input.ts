55b54272abd48375967c9086efc3a745757204ae
Subiniiie <subin57419@gmail.com>
2025-03-13 13:28:44 +0900

    searchbar 생성 중

--- src/styles/input.ts
+++ src/styles/input.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import { color } from "../store/color";
 
 export const InputWrapper = styled.input`
     type: "text;
     display: relative;
     width: 80%;
-    height: 30px
+    height: 30px;
+    color: ${color.searchBar};
+    border-radius: 5px;
+    border: none;
+    border: 10px solid ${color.searchBarBorder} !important;
 `
